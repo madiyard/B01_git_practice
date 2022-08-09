397e95cd55f79678de951c8c5ee1ee2b56bb0690
madiyard <icloudka@gmail.com>
2022-08-09 09:51:47 -0700

    Employee updated

--- src/day3/Employees.java
+++ src/day3/Employees.java
@@ -3,5 +3,7 @@ package day3;
 public class Employees {
     public static void main(String[] args) {
         System.out.println("some text");
+
+        System.out.println("Added new features");
     }
 }
